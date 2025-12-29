f31ece6976a20e7138e7044c8924e5bc57cf0e15
leah.digiteer@gmail.com <leah.digiteer@gmail.com>
2025-12-29 10:47:44 +0800

    realtime supabase

--- App.tsx
+++ App.tsx
@@ -1,3 +1,7 @@
+// Polyfills for React Native + Supabase Realtime
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { ProductProvider } from "./src/Screens/ProductContext";
